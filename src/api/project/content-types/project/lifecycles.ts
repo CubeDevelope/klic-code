d0963af9d39721f385f1c2ec0e17f0118ca75505
LeoLuca97 <37438630+CubeDevelope@users.noreply.github.com>
2025-06-07 23:02:07 +0200

    Add comment in projects' lifecycle

--- src/api/project/content-types/project/lifecycles.ts
+++ src/api/project/content-types/project/lifecycles.ts
@@ -1,21 +1,18 @@
 import { capitalize } from "../../../../utils/functions";
 
 export default {
+    /**
+     * TODO: Inserting a library into a project doesn't trigger the libraries' beforeUpdate/beforeCreate, 
+     * so when the relation is added, changed, or removed, 
+     * the corresponding library record must be checked and updated accordingly.
+     */
+
     async beforeCreate(event) {
         const { data } = event.params;
         data.name = capitalize(data.name);
-        /**
-         * TODO: In beforeCreate, check if the project is being linked to one or more libraries.
-         * If so, automatically set `isActive` to true. Otherwise, leave it as false (default).
-         */
     },
 
     async beforeUpdate(event) {
-        /**
-         * TODO: In beforeUpdate, determine if the list of associated libraries is being modified
-         * (e.g. libraries added or removed). Based on the final count after the update,
-         * set `isActive` to true if at least one library remains linked, or false if none.
-         */
     }
 
 
